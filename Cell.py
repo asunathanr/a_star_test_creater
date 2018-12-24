2c5afe358c685ec987ab8fdb100005b1d473b9f5
Nathan Robertson <absnathanr@gmail.com>
2018-12-24 07:48:31 -0600

    Renamed callback

--- Cell.py
+++ Cell.py
@@ -1,21 +1,29 @@
 from tkinter import Button
 
+"""
+File: Cell.py
+Author: Nathan Robertson
+A class that represents one cell on a grid. On click the cell changes color to indicate it is on/off depending
+on previous state.
+"""
+
+IS_CELL_COLOR = "#99ff99"
+IS_OBSTACLE_COLOR = "#555555"
+
 
 class Cell(Button):
     def __init__(self, master, pos, **kwargs):
-        self.IS_CELL_COLOR = "#99ff99"
-        self.IS_OBSTACLE_COLOR = "#555555"
-        self.current_color = self.IS_CELL_COLOR
+        self.current_color = IS_CELL_COLOR
         self.pos = pos
         super().__init__(master, kwargs)
-        super().configure(command=self.callback, bg=self.current_color)
+        super().configure(command=self.change_color, bg=self.current_color)
 
-    def callback(self):
-        if self.current_color == self.IS_CELL_COLOR:
-            self.current_color = self.IS_OBSTACLE_COLOR
+    def change_color(self):
+        if self.current_color == IS_CELL_COLOR:
+            self.current_color = IS_OBSTACLE_COLOR
         else:
-            self.current_color = self.IS_CELL_COLOR
+            self.current_color = IS_CELL_COLOR
         super().configure(bg=self.current_color)
 
     def is_obstacle(self) -> bool:
-        return self.current_color == self.IS_OBSTACLE_COLOR
+        return self.current_color == IS_OBSTACLE_COLOR
