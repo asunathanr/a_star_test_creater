46b3d4fcbd113a2449b098d48f1351e51ab1e09c
Nathan Robertson <absnathanr@gmail.com>
2018-12-24 07:37:53 -0600

    Renamed cell colors

--- Cell.py
+++ Cell.py
@@ -3,19 +3,19 @@ from tkinter import Button
 
 class Cell(Button):
     def __init__(self, master, pos, **kwargs):
-        self.FIRST_COLOR = "#99ff99"
-        self.SECOND_COLOR = "#555555"
-        self.current_color = self.FIRST_COLOR
+        self.IS_CELL_COLOR = "#99ff99"
+        self.IS_OBSTACLE_COLOR = "#555555"
+        self.current_color = self.IS_CELL_COLOR
         self.pos = pos
         super().__init__(master, kwargs)
         super().configure(command=self.callback, bg=self.current_color)
 
     def callback(self):
-        if self.current_color == self.FIRST_COLOR:
-            self.current_color = self.SECOND_COLOR
+        if self.current_color == self.IS_CELL_COLOR:
+            self.current_color = self.IS_OBSTACLE_COLOR
         else:
-            self.current_color = self.FIRST_COLOR
+            self.current_color = self.IS_CELL_COLOR
         super().configure(bg=self.current_color)
 
     def is_obstacle(self) -> bool:
-        return self.current_color == self.SECOND_COLOR
+        return self.current_color == self.IS_OBSTACLE_COLOR
